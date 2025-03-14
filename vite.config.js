cc512336daf0df65608c6a0a44c8ad533ed54270
Garrett Gee <31299429+grt812@users.noreply.github.com>
2025-03-14 16:50:16 -0400

    Fix base path URL to repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/dist', // 👈 Must match your GitHub repository name!
+  base: '/HARP-Website/', // 👈 Must match your GitHub repository name!
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
