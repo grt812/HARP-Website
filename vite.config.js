df12439f313c4a65f272cef4c8fd0c111f107840
grt812 <garrett.m.gee@gmail.com>
2025-03-17 22:44:37 -0400

    Add env variable for base url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
   plugins: [vue()],
-  base: '/HARP-Website/', // 👈 Must match your GitHub repository name!
+  base: process.env.VITE_BASE_URL, //Set base url in .env for testing purposes
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -14,4 +17,4 @@ export default defineConfig({
     outDir: 'dist', // Ensures build output goes to 'dist' folder
   },
   assetsInclude: ['**/*.JPG']
-})
+})}
